1873b8de3470e1993dcef0cb386974d36b67fd47
Michael Fortunato <Michael.N.Fortunato@gmail.com>
2024-02-27 13:32:51 -0500

    Update init.lua
    Turned off root detection. 
    
    Now it will always be the cwd of the process.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,11 @@
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
-
 -- bootstrap lazy.nvim, LazyVim and your plugins
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+-- LazyVim root dir detection
+-- Each entry can be:
+-- * the name of a detector function like `lsp` or `cwd`
+-- * a pattern or array of patterns like `.git` or `lua`.
+-- * a function with signature `function(buf) -> string|string[]`
+-- vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
+-- To disable root detection set to just "cwd"
+vim.g.root_spec = { "cwd" }
 require("config.lazy")
