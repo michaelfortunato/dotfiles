a447d7f9072ab0e7b5371b2649fbb75b5af83031
Michael Fortunato <michael.n.fortunato@gmail.com>
2024-03-22 15:42:04 -0400

    Updated nvim rootspec and added .gitconfig

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappin
 -- * the name of a detector function like `lsp` or `cwd`
 -- * a pattern or array of patterns like `.git` or `lua`.
 -- * a function with signature `function(buf) -> string|string[]`
--- vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
+vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
 -- To disable root detection set to just "cwd"
-vim.g.root_spec = { "cwd" }
+-- vim.g.root_spec = { "cwd" }
 require("config.lazy")
