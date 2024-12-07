bb8df6f7a5dced8a0d48e5f86eaaad590869c2fa
Michael Fortunato <michael.n.fortunato@gmail.com>
2024-12-06 21:03:08 -0600

    feat: mapping ; to [, while I still think its best. Is just not sustainable

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -10,12 +10,16 @@ map("n", "<c-\\>", function()
   Snacks.terminal(nil, { cwd = LazyVim.root() })
 end, { desc = "Terminal (Root Dir)" })
 map("t", "<C-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
---- NOTE: Very important swap. ; -> [ and ' ->]
 
-map({ "n", "v", "s", "o" }, ";", "[", { remap = true, desc = "For backwards textobject navigation" })
-map({ "n", "v", "s", "o" }, ";;", "[[", { noremap = true, desc = "For backwards textobject navigation" })
-map({ "n", "v", "s", "o" }, "'", "]", { remap = true, desc = "For forwards textobject navigation" })
-map({ "n", "v", "s", "o" }, "''", "]]", { noremap = true, desc = "For forwards textobject navigation" })
+--- NOTE: Very important swap. ; -> [ and ' ->]
+--- On second though This is a bad idea
+-- map({ "n", "v", "s", "o" }, ";", "[", { remap = true, desc = "For backwards textobject navigation" })
+-- map({ "n", "v", "s", "o" }, ";;", "[[", { noremap = true, desc = "For backwards textobject navigation" })
+-- map({ "n", "v", "s", "o" }, "g;", "g[", { remap = true, desc = "For forwards textobject navigation" })
+-- map({ "n", "v", "s", "o" }, "'", "]", { remap = true, desc = "For forwards textobject navigation" })
+-- map({ "n", "v", "s", "o" }, "''", "]]", { noremap = true, desc = "For forwards textobject navigation" })
+-- map({ "n", "v", "s", "o" }, "g'", "g]", { remap = true, desc = "For forwards textobject navigation" })
+
 map({ "n", "v", "o" }, "[s", "(", { desc = "For backwards (s)entece object navigation" })
 map({ "n", "v", "o" }, "]s", ")", { desc = "For forwards (s)entece object navigation" })
 
--- nvim/.config/nvim/lua/plugins/luasnip/make.lua
+++ nvim/.config/nvim/lua/plugins/luasnip/make.lua
@@ -7,76 +7,114 @@ local get_visual = function(args, parent)
 end
 
 local line_begin = require("luasnip.extras.expand_conditions").line_begin
-
 return {
   s(
-    { trig = "doc", snippetType = "autosnippet" },
+    { trig = "rdoc", snippetType = "autosnippet" },
     fmta(
       [[
-      CC=gcc
-      CFLAGS=""
-      DBGFLAGS := -g
-      COBJFLAGS := $(CFLAGS) -c
-      # path macros
-      BIN_DIR := bin
-      OBJ_DIR := obj
-      SRC_DIR := src
-      all:
-        <>
-      build <>:
-        <>
-      install <>:
-        <>
-      clean:
-        <>
-     ]],
+    # This Makefile is to run commands. While justfiles are better purposed
+    # It is unlikely Make will go away and it works well enough for my needs.
+    .PHONY: <> <> <> clean
+    <>:
+    \#techo 'TODO'
+    <>:
+    \t#echo 'TODO'
+    <>:
+    \t#echo 'TODO'
+    clean:
+    \t#echo 'TODO'
+   ]],
       {
-        i(1),
-        i(2),
-        i(3),
-        i(4),
-        i(5),
-        i(6),
-      }
-    ),
-    { condition = line_begin } --TODO: Condition should be begining of file!
-  ),
-  -- Make is not a runner, but it is lol. Use make as a runner here.
-  s(
-    { trig = "tdoc", snippetType = "autosnippet" },
-    fmta(
-      [[
-      all:
-        <>
-      build <>:
-        <>
-      install <>:
-        <>
-      clean:
-        <>
-     ]],
-      {
-        i(1),
-        i(2),
-        i(3),
-        i(4),
-        i(5),
-        i(6),
-      }
-    ),
-    { condition = line_begin }
-  ),
-  s(
-    { trig = "all", snippetType = "autosnippet" },
-    fmta(
-      [[
-      all:
-        <>
-     ]],
-      {
-        i(1),
+        rep(1),
+        rep(2),
+        rep(3),
+        i(1, "command1"),
+        i(2, "command2"),
+        i(3, "command3"),
       }
     ),
     { condition = line_begin }
   ),
 }
+
+-- return {
+--   s(
+--     { trig = "tdoc", snippetType = "autosnippet" },
+--     fmta([[foo<>]], i(1))
+--
+--     -- { condition = line_begin } --TODO: Condition should be begining of file!
+--   ),
+--   s(
+--     { trig = "doc", snippetType = "autosnippet" },
+--     fmta(
+--       [[
+--       CC=gcc
+--       CFLAGS=""
+--       DBGFLAGS := -g
+--       COBJFLAGS := $(CFLAGS) -c
+--       # path macros
+--       BIN_DIR := bin
+--       OBJ_DIR := obj
+--       SRC_DIR := src
+--       all:
+--         <>
+--       build <>:
+--         <>
+--       install <>:
+--         <>
+--       clean:
+--         <>
+--      ]],
+--       {
+--         i(1),
+--         i(2),
+--         i(3),
+--         i(4),
+--         i(5),
+--         i(6),
+--       }
+--     )
+--     -- { condition = line_begin } --TODO: Condition should be begining of file!
+--   ),
+--   -- Make is not a runner, but it is lol. Use make as a runner here.
+--   s(
+--     { trig = "rdoc", snippetType = "autosnippet" },
+--     fmta(
+--       [[
+--       # This Makefile is to run commands. While justfiles are better purposed
+--       # It is unlikely Make will go away and it works well enough for my needs.
+--       .PHONY: <> <> <>
+--       <>:
+--         # echo 'TODO'
+--       <>:
+--         # echo 'TODO'
+--       <>:
+--         # echo 'TODO'
+--       clean:
+--         # echo 'TODO'
+--      ]],
+--       {
+--         rep(1),
+--         rep(2),
+--         rep(3),
+--         i(1, "command1"),
+--         i(2, "command2"),
+--         i(3, "command3"),
+--       }
+--     ),
+--     { condition = line_begin }
+--   ),
+--   -- s(
+--   --   { trig = "all", snippetType = "autosnippet" },
+--   --   fmta(
+--   --     [[
+--   --     all:
+--   --       <>
+--   --    ]],
+--   --     {
+--   --       i(1),
+--   --     }
+--   --   ),
+--   --   { condition = line_begin }
+--   -- ),
+-- }
