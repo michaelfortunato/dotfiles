14afd374b7a2b109c4c37ee1d061e6d80259b98c
Michael Fortunato <michael.n.fortunato@gmail.com>
2024-12-19 13:29:09 -0600

    fix: disable image.nvim

--- nvim/.config/nvim/lua/plugins/image.nvim.lua
+++ nvim/.config/nvim/lua/plugins/image.nvim.lua
@@ -1,8 +1,12 @@
-return {
-  {
-    "3rd/image.nvim",
-    event = "VeryLazy",
-    opts = {},
-    build = false,
-  },
-}
+---NOTE: Disable image.nvim until it gets better
+if true then
+  return {}
+end
+-- return {
+--   {
+--     "3rd/image.nvim",
+--     event = "VeryLazy",
+--     opts = {},
+--     build = false,
+--   },
+-- }
