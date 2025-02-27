3d4983f2799dd7413cc2bc9349ca797465a04479
Michael Fortunato <michael.n.fortunato@gmail.com>
2025-02-26 23:51:33 -0600

    feat: powerline because its fun

--- Tacitus/.p10k.zsh
+++ Tacitus/.p10k.zsh
@@ -578,7 +578,7 @@
   typeset -g POWERLEVEL9K_DIRENV_FOREGROUND=3
   typeset -g POWERLEVEL9K_DIRENV_BACKGROUND=0
   # Custom icon.
-  # typeset -g POWERLEVEL9K_DIRENV_VISUAL_IDENTIFIER_EXPANSION='⭐'
+  typeset -g POWERLEVEL9K_DIRENV_VISUAL_IDENTIFIER_EXPANSION='🎛️'
 
   ###############[ asdf: asdf version manager (https://github.com/asdf-vm/asdf) ]###############
   # Default asdf color. Only used to display tools for which there is no color override (see below).
