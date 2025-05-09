6bc1039475b8c71d6951b34f786439c801e252ce
Michael Fortunato <michael.n.fortunato@gmail.com>
2025-05-08 20:29:10 -0500

    feat: rename nvim-lspconfig.lua to lsp.lua

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,87 @@
+--- Includes lsp, linting, and formatter configurations
+vim.lsp.set_log_level("ERROR")
+return {
+  {
+    -- LSP Configuration, note that some LSPs do formatting. It is
+    -- entirely up to them
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- add a keymap
+      keys[#keys + 1] = {
+        "gh",
+        function()
+          return vim.lsp.buf.hover()
+        end,
+        desc = "Hover",
+      }
+      keys[#keys + 1] = {
+        "gi",
+        function()
+          return vim.lsp.buf.implementation()
+        end,
+        desc = "Goto Implementation",
+      }
+    end,
+    ---@class PluginLspOpts
+    opts = {
+      --- Get that shi out of here!
+      inlay_hints = { enabled = false },
+      servers = {
+        texlab = {
+          --  keys = {
+          --    { "<Leader>K", "<plug>(vimtex-doc-package)", desc = "Vimtex Docs", silent = true },
+          --    -- Override [[ goto reference
+          --    { "[[", mode = { "n", "x", "o" }, "<plug>(vimtex-[[)", desc = "Vimtex Docs", silent = true },
+          --    { "]]", mode = { "n", "x", "o" }, "<plug>(vimtex-]])", desc = "Vimtex Docs", silent = true },
+          --  },
+        },
+        nixd = {
+          -- https://github.com/NixOS/nixfmt
+          settings = {
+            nixd = {
+              formatting = {
+                command = { "nixfmt" },
+              },
+              nixpkgs = {
+                -- For flake.
+                -- This expression will be interpreted as "nixpkgs" toplevel
+                -- Nixd provides package, lib completion/information from it.
+                -- Resource Usage: Entries are lazily evaluated, entire nixpkgs takes 200~300MB for just "names".
+                -- Package documentation, versions, are evaluated by-need.
+                -- Thanks! https://sbulav.github.io/vim/neovim-setting-up-nixd/
+                expr = "import (builtins.getFlake(toString ./.)).inputs.nixpkgs { }",
+              },
+            },
+          },
+        },
+        tinymist = {
+          settings = {
+            formatterMode = "",
+            -- formatterMode = "typstfmt",
+            -- do not fallb back to lsp formatting, as tinymist
+            -- runs its own fork of typstyle which confuses me
+            -- formatterMode = "typstyle",
+            -- formatterPrintWidth = 80,
+            -- TODO: Does this use typststyle?
+            lint = { enabled = true },
+          },
+        },
+      },
+    },
+  },
+  -- Formatting Configuration
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        typst = { "typstyle" },
+      },
+      formatters = {
+        typstyle = {
+          prepend_args = { "--wrap-text", "--column", "79" },
+        },
+      },
+    },
+  },
+}
