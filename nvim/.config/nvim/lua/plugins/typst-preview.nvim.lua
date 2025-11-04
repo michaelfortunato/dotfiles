1ca8f50a15655829e51f375647e8a55c105465f0
Michael Fortunato <michael.n.fortunato@gmail.com>
2025-11-04 12:48:23 -0600

    fix(typst-preview): hacky solution to get wasm cach ook on ff

--- nvim/.config/nvim/lua/plugins/typst-preview.nvim.lua
+++ nvim/.config/nvim/lua/plugins/typst-preview.nvim.lua
@@ -811,17 +811,19 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+---@type LazySpec
 return {
   "chomosuke/typst-preview.nvim",
   ft = "typst",
   opts = {
-    -- debug = true,
-    dependencies_bin = {
-      -- ["tinymist"] = vim.fn.stdpath("data") .. "/mason/bin/tinymist",
-      -- My fork
-      -- ["tinymist"] = "/Users/michaelfortunato/projects/tinymist/target/release/tinymist",
-    },
-  }, -- lazy.nvim will implicitly calls `setup {}`
+    -- HACK: The issue is on firefox non private browser that opening
+    -- 127.0.0.1:49810 caused it to be slow for some reason as compared
+    -- to firefox private or safari regular. This fixes it though
+    port = 49810,
+    open_cmd = "open http://localhost:49810",
+    debug = false,
+    dependencies_bin = { ["tinymist"] = "tinymist" },
+  },
   keys = { {
     "<leader>tp",
     "<Cmd>TypstPreviewToggle<CR>",
