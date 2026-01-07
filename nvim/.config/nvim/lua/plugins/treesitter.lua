2969588da8dfe3463254e0738cf687f05a60e2f7
Michael Fortunato <michael.n.fortunato@gmail.com>
2026-01-07 10:21:37 -0600

    feat: Address #3 - Swap nvim-surround for mini

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -185,4 +185,29 @@ return {
       end)
     end,
   },
+  {
+    ---@module  "mini.surround"
+    "echasnovski/mini.surround",
+    config = function()
+      require("mini.surround").setup({
+        mappings = {
+          add = "ys",
+          delete = "ds",
+          find = "",
+          find_left = "",
+          highlight = "",
+          replace = "cs",
+
+          -- Add this only if you don't want to use extended mappings
+          suffix_last = "",
+          suffix_next = "",
+        },
+        search_method = "cover_or_next",
+      })
+      vim.keymap.del("x", "ys")
+      vim.keymap.set("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
+      -- Make special mapping for "add surrounding for line"
+      -- vim.keymap.set("n", "yss", "ys_", { remap = true })
+    end,
+  },
 }
