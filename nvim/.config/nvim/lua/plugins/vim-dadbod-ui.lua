33c6a99d3ed1461eb4174b432dca5ea51d800d1e
Michael Fortunato <michael.n.fortunato@gmail.com>
2026-01-15 17:02:52 -0600

    feat: sql ui in neovim

---
+++ nvim/.config/nvim/lua/plugins/vim-dadbod-ui.lua
@@ -0,0 +1,21 @@
+-- So sick
+-- vim.g.dbs = {
+-- 	{ name = "exp1", url = "sqlite:///Users/michaelfortunato/projects/SymD/data/Exp1/output.db" },
+-- }
+return {
+  "kristijanhusak/vim-dadbod-ui",
+  dependencies = {
+    { "tpope/vim-dadbod", lazy = true },
+    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+  },
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
+}
