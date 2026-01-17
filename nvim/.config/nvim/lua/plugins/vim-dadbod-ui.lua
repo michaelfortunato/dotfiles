4c410df6bdf5b11a11087d519e437483bb028b30
Michael Fortunato <michael.n.fortunato@gmail.com>
2026-01-16 18:59:30 -0600

    chore: remove unused dadbod completion

--- nvim/.config/nvim/lua/plugins/vim-dadbod-ui.lua
+++ nvim/.config/nvim/lua/plugins/vim-dadbod-ui.lua
@@ -1,12 +1,21 @@
--- So sick
+-- This could be worth looking into as a lua native
+-- https://github.com/kndndrj/nvim-dbee?tab=readme-ov-file-- So sick
 -- vim.g.dbs = {
 -- 	{ name = "exp1", url = "sqlite:///Users/michaelfortunato/projects/SymD/data/Exp1/output.db" },
 -- }
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("sql-dadbod", { clear = true }),
+  pattern = "sql",
+  callback = function(event)
+    vim.keymap.set("n", "<Enter>", "<Plug>(DBUI_ExecuteQuery)", { buffer = event.buf })
+  end,
+})
 return {
   "kristijanhusak/vim-dadbod-ui",
   dependencies = {
     { "tpope/vim-dadbod", lazy = true },
-    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+    -- { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
   },
   cmd = {
     "DBUI",
