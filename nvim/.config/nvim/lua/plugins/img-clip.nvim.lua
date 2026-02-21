08ca5f1e14c7e09766b33211b6f1231d2694e771
Michael Fortunato <michael.n.fortunato@gmail.com>
2026-02-21 15:10:24 -0600

    Improve img pasting in neovim

--- nvim/.config/nvim/lua/plugins/img-clip.nvim.lua
+++ nvim/.config/nvim/lua/plugins/img-clip.nvim.lua
@@ -3,17 +3,43 @@ return {
   lazy = false,
   opts = {
     default = {
-      verbose = false,
       -- file and directory options
       dir_path = function()
-        return LazyVim.root() .. "/assets"
+        local root = LazyVim.root() or vim.uv.cwd()
+        return root .. "/assets"
       end, ---@type string | fun(): string
+      copy_images = true,
     },
+    -- Options that ONLY apply while dragging/dropping (img-clip overrides vim.paste)
+    drag_and_drop = {
+      verbose = false,
+      prompt_for_file_name = true,
+      insert_mode_after_paste = false,
+    },
+
+    download_images = true,
     -- add options here
     -- or leave it empty to use the default settings
     filetypes = {
       typst = {
-        template = [[image("assets/$FILE_NAME", width: 80%)]], ---@type string | fun(context: table): string
+        template = function(context)
+          return [[#image("/assets/$FILE_NAME", width: 80%)]]
+        end,
+      },
+      mdx = {
+        download_images = true,
+      },
+      javascript = {
+        template = '<img src="$FILE_PATH" alt="$CURSOR">', ---@type string | fun(context: table): string
+      },
+      typescript = {
+        template = '<img src="$FILE_PATH" alt="$CURSOR">', ---@type string | fun(context: table): string
+      },
+      javascriptreact = {
+        template = '<img src="$FILE_PATH" alt="$CURSOR" />', ---@type string | fun(context: table): string
+      },
+      typescriptreact = {
+        template = '<img src="$FILE_PATH" alt="$CURSOR" />', ---@type string | fun(context: table): string
       },
     },
   },
