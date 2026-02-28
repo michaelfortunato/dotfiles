5fe2eb5bac47d0fe99b46ca5fb16ebab49d6a17c
Michael Fortunato <michael.n.fortunato@gmail.com>
2026-02-27 21:33:22 -0600

    Delegate to default dev config (set in lazy.lua)
    Dev default settings like path are configured in config/lazy.lua

--- nvim/.config/nvim/lua/plugins/smart-splits.nvim.lua
+++ nvim/.config/nvim/lua/plugins/smart-splits.nvim.lua
@@ -1,12 +1,6 @@
 return {
   "michaelfortunato/smart-splits.nvim",
-  dev = {
-    ---@type string | fun(plugin: LazyPlugin): string directory where you store your local plugin projects
-    path = "~/projects/neovim-plugins/",
-    ---@type string[] plugins that match these patterns will use your local versions instead of being fetched from GitHub
-    patterns = {}, -- For example {"folke"}
-    fallback = true, -- Fallback to git when local plugin doesn't exist
-  },
+  dev = true,
   build = "./kitty/install-kittens.bash",
   lazy = false,
 }
