199ceafb36e452970f7fcee5dfb86d020fa859f9
Michael Fortunato <michael.n.fortunato@gmail.com>
2026-02-27 23:27:44 -0600

    Have typst preview bring the existing preview over if it already exists

--- nvim/.config/nvim/lua/plugins/typst-preview.nvim.lua
+++ nvim/.config/nvim/lua/plugins/typst-preview.nvim.lua
@@ -11,7 +11,23 @@ return {
     --open_cmd = "open http://localhost:49811",
     port = 41798,
     host = "127.0.0.1",
-    open_cmd = "open -g -a 'Typst Preview' '%s'",
+    -- open_cmd = "open -g -a 'Typst Preview' '%s'",
+    open_cmd = [[bash -lc '
+URL="$1"
+SW="PS"
+
+if command -v aerospace >/dev/null 2>&1; then
+  CW="$(aerospace list-workspaces --focused --format "%%{workspace}" 2>/dev/null | head -n1 || true)"
+  WID="$(aerospace list-windows --workspace "$SW" --format "%%{window-id}|%%{window-title}" | rg -m1 "\\|Typst Preview$" || true)"
+
+  if [ -n "${CW:-}" ] && [ -n "${WID:-}" ] >/dev/null 2>&1; then
+    aerospace move-node-to-workspace --window-id "$WID" "$CW" >/dev/null 2>&1 || true
+    exit 0
+  fi
+fi
+
+open -a "Typst Preview" "$URL"
+' _ '%s']],
     debug = true,
     dependencies_bin = { ["tinymist"] = "tinymist" },
   },
