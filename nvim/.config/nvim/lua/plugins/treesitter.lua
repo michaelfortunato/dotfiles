94199c85ba08df8a0cb95f7ca347dd4947cdd6b7
Michael Fortunato <michael.n.fortunato@gmail.com>
2026-05-30 13:05:42 -0400

    Add mise TOML run injections

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -43,6 +43,11 @@ return {
 
       query.add_predicate = orig_add_predicate
       query.add_directive = orig_add_directive
+      query.add_predicate("is-mise?", function(_, _, bufnr, _)
+        local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+        local filename = vim.fn.fnamemodify(filepath, ":t")
+        return string.match(filename, ".*mise.*%.toml$") ~= nil
+      end, { force = true, all = false })
     end,
     opts = {
       indent = {
